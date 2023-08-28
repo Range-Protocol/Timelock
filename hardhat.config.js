2e4b5449c4d05a8f4e4fa7a2931042d7916cc496
Don Bateman <bateman1392@gmail.com>
2023-08-28 14:21:38 +0800

    downgrade pragma version to solidity 0.8.19

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-verify");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-	etherscan: {},
+	etherscan: {
+		apiKey: {
+			"mantle": "abc", //random value
+		},
+		customChains: [
+			{
+				network: "mantle",
+				chainId: 5000,
+				urls: {
+					apiURL: "https://explorer.mantle.xyz/api",
+					browserURL: "https://explorer.mantle.xyz",
+				},
+			},
+		],
+	},
 	networks: {
 		mantle: {
-			url: ""
+			url: "https://rpc.mantle.xyz",
+			chainId: 5000,
 		}
 	},
 	solidity: {
 		compilers: [
 			{
-				version: "0.8.20",
+				version: "0.8.19",
 				settings: {
 					optimizer: {enabled: true, runs: 200},
 				},
